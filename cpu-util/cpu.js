e2def980339de439ff478462bda8b39b148aa43a
Alexander Clines <injpk23@gmail.com>
2017-02-20 10:21:55 -0600

    Math now works

--- cpu-util/cpu.js
+++ cpu-util/cpu.js
@@ -4,11 +4,7 @@ function cpuUtil(c, p, n){
     document.getElementById("cpuUtil").value = (1- Math.pow(p,n));
   } else if(!p){
     console.log("Calc P");
-    console.log(c,n);
-    console.log(Math.log(25)/Math.log(2));
-    console.log(Math.log(1-c));
-    console.log(Math.log(n));
-    document.getElementById("iotime").value = Math.log(1-c)/Math.log(n);
+    document.getElementById("iotime").value = Math.pow((1-c),(1/n))
   } else if(!n){
     console.log("Calc N");
     document.getElementById("processNumber").value = Math.log(1-c)/Math.log(p);
