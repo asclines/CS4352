d6874a32b5bf038398c16bf2b6c5a71c688523ab
Alexander Clines <injpk23@gmail.com>
2017-02-21 11:51:42 -0600

    Made it obvious to find point. Cant drag map now. On purpose.

--- cpu-util/cpu.js
+++ cpu-util/cpu.js
@@ -1,10 +1,10 @@
 function cpuUtil(c, p, n){
   if(!c){
     console.log("Calc CPU Util");
-    document.getElementById("cpuUtil").value = (1- Math.pow(p,n));
+    document.getElementById("cpuUtil").value = (1- Math.pow(p,n).toFixed(3));
   } else if(!p){
     console.log("Calc P");
-    document.getElementById("iotime").value = Math.pow((1-c),(1/n))
+    document.getElementById("iotime").value = Math.pow((1-c),(1/n)).toFixed(3);
   } else if(!n){
     console.log("Calc N");
     document.getElementById("processNumber").value = Math.ceil(Math.log(1-c)/Math.log(p));
